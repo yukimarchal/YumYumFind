35890963364444cead884a07c9ba1777e5394bcb
Louis Delleur <louis.delleur@edu.technobel.be>
2024-07-23 16:40:01 +0200

    c plus PT

--- louis/Content/duck.js
+++ louis/Content/duck.js
@@ -11,28 +11,28 @@ window.Duck = {
     "d002": {
         name : "Dark Duck",
         type : DuckTypes.dev,
-        scr : "/louis/images/characters/Duck/d002.png" ,
+        src : "/louis/images/characters/Duck/d002.png" ,
         icon:"/louis/images/icons/spicy.png" ,
     },
 
     "d001": {
         name : "Js Duck",
         type : DuckTypes.dev,
-        scr : "/louis/images/characters/Duck/d001.png" ,
+        src : "/louis/images/characters/Duck/d001.png" ,
         icon:"/louis/images/icons/chill.png" ,
     },
 
     "m001": {
         name : "Ducky Duck",
         type : DuckTypes.mare,
-        scr : "/louis/images/characters/Duck/m001.png" ,
+        src : "/louis/images/characters/Duck/m001.png" ,
         icon:"/louis/images/icons/fungi.png" ,
     },
 
     "p001": {
         name : "Dark Duck",
         type : DuckTypes.plastique,
-        scr : "/louis/images/characters/Duck/P001.png" ,
+        src : "/louis/images/characters/Duck/P001.png" ,
         icon:"/louis/images/icons/veggie.png" ,
     },
 
