60175e1fca6a4455e93819a5ff11316dbad95384
Steven Hanse (Malashuy) <steven.hanse@live.be>
2024-07-23 16:01:24 +0200

    Correction de la barre de point de vie deu joeur

--- steven/js/PlayerHealthBar.js
+++ steven/js/PlayerHealthBar.js
@@ -19,12 +19,18 @@ export class PlayerHealthBar {
     static draw = (maxhp, hp, x = -2, y = 0) => {
         let ctx = GameZone.context
 
-        let newMapHp = 43
+        let newMapHp = 45
         let hpRatio = hp / maxhp
         let newHp = Math.round(newMapHp * hpRatio)
 
-        for (let i = 0; i < newHp; i++)
-            ctx.drawImage(PlayerHealthBar.#health_bar["pixel"], x+33+i, y+12)
+        for (let i = 0; i < newHp; i++) {
+            if (i === 43 || i === 44) {
+                ctx.drawImage(PlayerHealthBar.#health_bar["pixel"], 0, 0, 1, 3, x + 33 + i, y + 12, 1, 3)
+            }
+            else {
+                ctx.drawImage(PlayerHealthBar.#health_bar["pixel"], x+33+i, y+12)
+            }
+        }
 
         ctx.drawImage(PlayerHealthBar.#health_bar["frame"], x, y)
     }
